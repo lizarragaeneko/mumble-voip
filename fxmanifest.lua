d7687e6416feaafd6c66ba0dd03e54a51187554e
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-07-25 17:04:44 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version "adamant"
+game "gta5"
+
+name "mumble-voip"
+version "1.3"
+
+ui_page "ui/index.html"
+
+files {
+	"ui/index.html",
+	"ui/mic_click_on.ogg",
+	"ui/mic_click_off.ogg",
+}
+
+shared_scripts {
+	"config.lua",
+}
+
+client_scripts {
+    "client.lua",
+}
+
+server_scripts {
+    "server.lua",
+}
+
+provide "tokovoip_script"
